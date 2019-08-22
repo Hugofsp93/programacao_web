9eb31a563332f4bc505021e9eaf6070787579fc3
Hugo Felipe <hugo@roadmaps.com.br>
2019-08-22 14:35:47 +0000

    encontro III 1 ao 6

---
+++ encontro_III/main.js
@@ -0,0 +1,135 @@
+// questão 01
+var hello_world = document.getElementById('question_1');
+hello_world.textContent = 'Hello world!';
+
+// questão 02
+var q2 = document.getElementById('question_2');
+q2.textContent = 'Frase elaborada para a questão 02!';
+
+// questão 03
+var question_3 = document.getElementById('question_3');
+question_3.onclick = function(){
+  alert(2 + 2);
+}
+
+// questão 04
+document.getElementById("btn-exibir-nome").addEventListener("click", function () {
+  const nomeCompleto = document.getElementById('nome-completo').value;
+  alert('O nome completo é: ' + nomeCompleto);
+});
+
+// questão 05
+document.getElementById("btn-letras").addEventListener("click", function () {
+  var qtdLetras = document.getElementById('qtd-letras').value.length;
+  alert('O nome possui ' + qtdLetras + ' letras');
+});
+
+// questão 06
+document.getElementById("my-photo").src = '../encontro_III/acesa.jpg';
+
+// questão 07
+
+
+// questão 08
+
+
+// questão 09
+
+
+// questão 10
+
+
+// questão 11
+
+
+// questão 12
+
+
+// questão 13
+
+
+// questão 14
+
+
+// questão 15
+
+
+// questão 16
+
+
+// questão 17
+
+
+// questão 18
+
+
+// questão 19
+
+
+// questão 20
+
+
+// questão 21
+
+
+// questão 22
+
+
+// questão 23
+
+
+// questão 24
+
+
+// questão 25
+
+
+// questão 26
+
+
+// questão 27
+
+
+// questão 28
+
+
+// questão 29
+
+
+// questão 30
+
+
+// questão 31
+
+
+// questão 32
+
+
+// questão 33
+
+
+// questão 34
+
+
+// questão 35
+
+
+// questão 36
+
+
+// questão 37
+
+
+// questão 38
+
+
+// questão 39
+
+
+// questão 40
+
+
+// questão 41
+
+
+// questão 42
